17f1ffbb2a0bb515f794aa56f6888b49230fbf9b
khokhar505 <62996108+khokhar505@users.noreply.github.com>
2020-12-20 20:37:33 +0500

    Add files via upload

--- try.py
+++ try.py
@@ -1,1 +1,20 @@
+_author_ = "taimoor"
 
+print("what is your name?")
+
+user_name = input()
+
+print("hello, "+ user_name)
+
+print("what is your age?")
+
+user_age = input()
+
+user_age = int(user_age)
+
+if user_age > 18:
+    print("fine")
+else:
+    print("tu kaka chuti kar!!!")
+
+print()
